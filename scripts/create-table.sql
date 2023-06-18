4c8a4991074e2972c5a0c679c64b0bfa94c1b3a4
MarioRP-01 <marlorosadron@gmail.com>
2023-06-18 21:51:59 +0200

    Fix init-db script
    - Changed clothing table's file_name to VARCHAR to store image
      extensions.
    - Change from pgdata to pg_data inside the container.
    - Fix variable usage inside the bash script

--- scripts/create-table.sql
+++ scripts/create-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS clothing (
     id SERIAL PRIMARY KEY,
-    file_name UUID UNIQUE,
+    file_name VARCHAR(255) UNIQUE,
     label VARCHAR(255),
     size VARCHAR(10),
     kids BOOLEAN
--- scripts/load-data.sql
+++ scripts/load-data.sql
@@ -2,7 +2,7 @@ DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM clothing LIMIT 1) THEN
         COPY clothing (file_name, label, size, kids)
-        FROM '/var/lib/postgresql/data/pgdata/data.csv'
+        FROM '/var/lib/postgresql/data/pg_data/data.csv'
         DELIMITER ',' CSV HEADER;
     END IF;
 END 
